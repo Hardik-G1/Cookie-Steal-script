b07bdf9482b48d78144368b75e988d74bcb7b7c2
Hardik Gupta <67197887+Hardik-G1@users.noreply.github.com>
2021-08-12 09:25:40 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,2 @@
+EDUCATION PURPOSE ONLY
 A very basic Cookie-Steal-script with wifi passwords capture, kelogging, killing taskmanager and cmd if tried to kill process and mailing data at specific interval.
